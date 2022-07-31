68061db7990a2853098496c66e26e5c69fc22ac0
Jesus <deonatto@gmail.com>
2022-07-31 17:05:57 +0100

    change description

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -10,8 +10,7 @@ const Intro = () => {
             </div>
             <div className = "right">
                 <div className="wrapper">
-                    <h2>Hi There, I´m</h2>
-                    <h1>Jesus Gonçalves.</h1>
+                    <h2>Hi There, my name is Jesus Goncalves and i´m a frontend developer.</h2>
                     <h3>To see my work, click below.</h3>
                 </div>
                 <a href = "#portfolio" alt="">
